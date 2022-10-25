c06a51e6dcf189ba883287244a2f08304a35ecbf
wlad78787 <tghhspirt@gmail.com>
2022-10-25 14:25:57 +0300

    nc<-6l.3

--- laba 4-5/Program.cs
+++ laba 4-5/Program.cs
@@ -66,18 +66,39 @@ class Program
         
         /*Дан файл целых чисел, содержащий более 50 элементов. Уменьшить его
     размер до 50 элементов, удалив из файла необходимое количество начальных элементов.*/
-        void work_6_1()
-        {
-            Console.WriteLine("222");
+        void work_6_1() {
+            var text = "";
+            using (StreamReader str = new StreamReader(path:"Test.txt")) {
+                text = str.ReadToEnd();
+            }
+            var lst = text.Split(" ").ToList();
+            while (lst.Count > 50) lst.RemoveAt(0);
+            text = lst.Aggregate("", (current, t) => current + (t + " "));
+            using (StreamWriter streamWriter = new StreamWriter(path: "Test.txt")) {
+                streamWriter.Write(text);
+            }
         }
 
         /*Дан строковый файл, содержащий даты в формате «день/месяц/год», причем
     под день и месяц отводится по две позиции, а под год — четыре (например, «16/04/2001»).
     Создать два файла целых чисел, первый из которых содержит значения месяцев, а второй
     — значения лет для дат из исходного строкового файла (в обратном порядке).*/
-        void work_6_2()
-        {
-            Console.WriteLine("222");
+        void work_6_2() {
+            var text = "";
+            using (StreamReader streamReader = new StreamReader(path: "Test.txt")) {
+                text = streamReader.ReadToEnd();
+            }
+            var lst = text.Split(" ").ToList();
+            var mounth = lst.Select(t => t.Split("/")[1]).ToList();
+            var years = lst.Select(t => t.Split("/")[2]).ToList();
+            years.Reverse();
+            mounth.Reverse();
+            using (StreamWriter streamWriter = new StreamWriter(path:"Mounths.txt")) {
+                streamWriter.Write(mounth.Aggregate("", (current, t) => current + (t + " ")));
+            }
+            using (StreamWriter streamWriter = new StreamWriter(path:"Years.txt")) {
+                streamWriter.Write(years.Aggregate("", (current, t) => current + (t + " ")));
+            }
         }
 
         /*Дан файл вещественных чисел, содержащий ненулевую часть
@@ -98,8 +119,8 @@ class Program
             [6] = work_6_3,
         };
         Console.WriteLine("Какое задание делаем?\n" + "1)4 лаба - задание 1\n" + "2)4 лаба - задание 2\n" +
-                          "3)5 лаба - задание 1\n" + "5)6 лаба - задание 1\n" + "6)6 лаба - задание 2\n" +
-                          "4)6 лаба - задание 3");
+                          "3)5 лаба - задание 1\n" + "4)6 лаба - задание 1\n" + "5)6 лаба - задание 2\n" +
+                          "6)6 лаба - задание 3");
         int key = (Funcs.ToInt(Console.ReadLine()));
         if (Works.ContainsKey(key))Works[key]();
         else Console.WriteLine("Не такого задания");
